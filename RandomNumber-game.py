5c12ecdac6a8a89b4350b3cb963d50cf2c1e544c
apexa11 <apexachoxi7@gmail.com>
2017-05-10 16:45:02 -0400

    RandomNumber-game

--- RandomNumber-game.py
+++ RandomNumber-game.py
@@ -1,15 +1,45 @@
 import random
 
-  #generate a number between 1 and 10
-secret_num = random.randint(1,10)
+# safely mane an in
+# limit guesses
+# too high message
+# too low message
+# play again
 
-while True:
-    #get a number guess from user
-    my_num =int(input("Guess number between 1 and 10:"))
+def game():
 
-    #compare guess to secret number
-    if my_num == secret_num:
-        print("You got it!!my number was {}".format(secret_num))
-        break
+    # generate a random number between 1 - 10
+    secret_num = random.randint(1, 10)
+
+    guesses = []
+
+    while len(guesses) < 5:
+
+        try:
+            # get a number guess from the player
+            guess = int(input("Guess a number between 1 and 10: "))
+        except ValueError:
+            print("{} isn't a number!".format(guess))
+        else:
+
+            # compare guees to secret number
+            if guess == secret_num:
+                print("You got it! My number was {}".format(secret_num))
+                break
+            elif guess < secret_num:
+                print("My number is higher than {}".format(guess))
+
+            else:
+                print("My number is lower than {}".format(guess))
+            guesses.append(guess)
+            # print hit/miss
     else:
-              print("That's not it!!")
+        print("You didn't get it! My number was {}".format(secret_num))        
+
+    play_again = input("Do you want to play again? Y/n ")
+    if play_again.upper == 'Y':
+        game()
+    else:
+        print("Bye!")
+
+game()
